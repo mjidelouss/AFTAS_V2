acdec7d38175269adaf1855e94c4a4447f59f1a3
mjidelouss <mjid.elouss@gmail.com>
2024-02-12 09:47:53 +0100

    Added Enums (TokenType, Role, Privilege)

---
+++ AFTAS/src/main/java/com/example/aftas/enums/Privilege.java
@@ -0,0 +1,9 @@
+package com.example.aftas.enums;
+
+public enum Privilege {
+    READ_PRIVILEGE,
+    WRITE_PRIVILEGE,
+    DELETE_PRIVILEGE,
+    UPDATE_PRIVILEGE,
+    ;
+}
---
+++ AFTAS/src/main/java/com/example/aftas/enums/TokenType.java
@@ -0,0 +1,5 @@
+package com.example.aftas.enums;
+
+public enum TokenType {
+    BEARER
+}
